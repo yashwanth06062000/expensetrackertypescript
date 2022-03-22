e91a7bf95d4f849f19b930c3b47897deea78d28c
yashwanth06062000 <yashwanthkrishna150@gmail.com>
2022-03-22 10:30:55 +0530

    alltypescript files

---
+++ workable/controllers/authorization.ts
@@ -0,0 +1,26 @@
+// const jwt = require('jsonwebtoken')
+import jwt from 'jsonwebtoken';
+// const user=require("../models/usersignup")
+import user from '../models/usersignup';
+
+// require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default function authenticate async(req, res, next) => {
+    try {
+        const token = req.header('authorization')
+        console.log(token)
+       
+        const userId = Number(jwt.verify(token, process.env.Tokensecrect))
+       await user.findByPk(userId).then((user)=>{
+        
+            req.user =user
+        next()
+        }).catch(err=>console.log(err))
+        
+    }catch(err) {
+        console.log(err)
+        return res.status(404).json({message: 'from authentication', success: false})
+    }
+}
